1d1402cb257c7bcae67e10cfef590f849a47d802
kaiesmahmud <kaiesmahmud@gmail.com>
2024-02-20 19:45:42 +0600

    sticky navbar added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+	import Navbar from './../lib/components/Navbar.svelte';
+    import Footer from '../lib/components/Footer.svelte';
     import "../app.css";
-  </script>
-  
-  <slot />
+</script>
+<div class="flex items-center justify-center   ">
+    <div class=" relative w-[100vw] md:max-w-[3000px] flex flex-col  bg-slate-200 z-0   ">
+        <div class=" sticky top-0 z-[100000] backdrop-blur-2xl shadow-lg">
+            <Navbar/>
+        </div>
+        <slot />
+        <Footer/>
+    </div>
+</div>
