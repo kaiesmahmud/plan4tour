68e1b4b5a4d1d3b90d4752113ce05c18ccb4c825
kaiesmahmud <kaiesmahmud@gmail.com>
2024-02-29 16:35:28 +0600

    working on single hotel describe page

---
+++ src/routes/hotel-booking/[hotel]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import SlidingImages from "../../../lib/components/SlidingImages.svelte";
+
+
+</script>
+<main class="bg-indigo-100">
+    <div>
+        Hotel Description
+    </div>
+    <SlidingImages/>
+
+</main>
