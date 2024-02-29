39b6c02146f710fac2562ca0b2d8b0ecba05c85a
kaiesmahmud <kaiesmahmud@gmail.com>
2024-02-29 14:38:54 +0600

    hotel booking page updated

--- src/routes/hotel-booking/+page.svelte
+++ src/routes/hotel-booking/+page.svelte
@@ -1,6 +1,116 @@
 <script>
+    import { hotels } from "../../lib/constants/hotels";
 
+    const facilities = [
+        {
+            name: "Air Condition",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256" {...$$props}>
+                    <path fill="currentColor" d="M233 135a60 60 0 0 0-89.62-35.45l16.39-65.44a8 8 0 0 0-3.45-8.68a60 60 0 1 0-60.63 103.48l-64.87 18.53a8 8 0 0 0-5.8 7.32a60 60 0 0 0 44.42 60.66a60.52 60.52 0 0 0 15.62 2.07a60.07 60.07 0 0 0 59.88-62l48.48 46.92a8 8 0 0 0 9.25 1.35A60 60 0 0 0 233 135m-121-7a16 16 0 1 1 16 16a16 16 0 0 1-16-16M80 76a44 44 0 0 1 62.75-39.82L127.77 96a32 32 0 0 0-27.92 16.8A43.85 43.85 0 0 1 80 76m27 119.57a44 44 0 0 1-65.86-34.43l59.31-16.94A32 32 0 0 0 128 160h.91A43.82 43.82 0 0 1 107 195.57m106.17-23a43.92 43.92 0 0 1-13 14.14l-44.32-42.89a31.91 31.91 0 0 0-.59-32.57a44 44 0 0 1 57.91 61.32" />
+                </svg>`,
+        },
+        {
+            name: "City Center",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+                    <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m7 9.01l.01-.011M11 9.01l.01-.011M7 13.01l.01-.011m3.99.011l.01-.011M7 17.01l.01-.011m3.99.011l.01-.011M15 21H3.6a.6.6 0 0 1-.6-.6V5.6a.6.6 0 0 1 .6-.6H9V3.6a.6.6 0 0 1 .6-.6h4.8a.6.6 0 0 1 .6.6V9m0 12h5.4a.6.6 0 0 0 .6-.6V9.6a.6.6 0 0 0-.6-.6H15m0 12v-4m0-8v4m0 0h2m-2 0v4m0 0h2" />
+                </svg>`,
+        },
+        {
+            name: "Hot Tub",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32" {...$$props}>
+	<path fill="currentColor" d="M11.219 4.375C10.39 5.398 10.008 6.273 10 7.125c-.008.852.371 1.512.625 1.938c.254.425.367.64.375.874c.008.235-.07.645-.688 1.438l1.563 1.25c.781-1.008 1.152-1.914 1.125-2.75c-.027-.836-.41-1.434-.656-1.844c-.246-.41-.348-.629-.344-.875c.004-.246.11-.703.781-1.531zm5 0C15.39 5.398 15.008 6.273 15 7.125c-.008.852.371 1.512.625 1.938c.254.425.367.64.375.874c.008.235-.07.645-.688 1.438l1.563 1.25c.781-1.008 1.152-1.914 1.125-2.75c-.027-.836-.41-1.434-.656-1.844c-.246-.41-.348-.629-.344-.875c.004-.246.11-.703.781-1.531zm5 0C20.39 5.398 20.008 6.273 20 7.125c-.008.852.371 1.512.625 1.938c.254.425.367.64.375.874c.008.235-.07.645-.688 1.438l1.563 1.25c.781-1.008 1.152-1.914 1.125-2.75c-.027-.836-.41-1.434-.656-1.844c-.246-.41-.348-.629-.344-.875c.004-.246.11-.703.781-1.531zM3 14v2h1.188l1.53 7.594v.031a3.062 3.062 0 0 0 2 2.219L7 28h2l.656-2h12.688L23 28h2l-.719-2.156c1.047-.32 1.86-1.16 2.094-2.219v-.031L27.813 16H29v-2zm3.219 2h19.593l-1.406 7.219c-.117.433-.484.781-1 .781H8.688c-.536 0-.899-.355-1-.813z" />
+</svg>`,
+        },
+        {
+            name: "Lobby",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+	<path fill="currentColor" d="M18.444 18H5.556a3.57 3.57 0 0 1-.806-.092V19a.75.75 0 0 1-1.5 0v-1.849A3.548 3.548 0 0 1 2 14.444V12a2 2 0 1 1 4 0v1.2a.8.8 0 0 0 .8.8h10.4a.8.8 0 0 0 .8-.8V12a2 2 0 1 1 4 0v2.444a3.548 3.548 0 0 1-1.25 2.707V19a.75.75 0 0 1-1.5 0v-1.092a3.57 3.57 0 0 1-.806.092" />
+	<path fill="currentColor" d="M4.007 9c.008-.328.027-.566.07-.78A4 4 0 0 1 7.22 5.077C7.606 5 8.07 5 9 5h6c.93 0 1.394 0 1.78.077a4 4 0 0 1 3.143 3.143c.043.214.062.452.07.78H20v1a2 2 0 0 0-2 2v1.2a.8.8 0 0 1-.8.8H6.8a.8.8 0 0 1-.8-.8V12a2 2 0 0 0-2-2V9z" opacity="0.5" />
+</svg>`,
+        },
+        {
+            name: "24 Hour Security",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48" {...$$props}>
+	<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="m24 24l5.303-5.303A7.5 7.5 0 1 0 31.5 24" />
+	<path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M24 43.5c9.043-3.117 15.489-10.363 16.5-19.589a79.362 79.362 0 0 0-.071-12.027a2.541 2.541 0 0 0-2.468-2.366c-4.091-.126-8.846-.808-12.52-4.427a2.052 2.052 0 0 0-2.881 0c-3.675 3.619-8.43 4.301-12.52 4.427a2.541 2.541 0 0 0-2.468 2.366A79.362 79.362 0 0 0 7.5 23.911C8.511 33.137 14.957 40.383 24 43.5" />
+</svg>`,
+        },
+        {
+            name: "Wifi",
+            icon: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+	<path fill="currentColor" d="m6.35 15.35l-1.023-1.035q1.334-1.345 3.056-2.06q1.722-.717 3.615-.717q1.894 0 3.618.723q1.724.722 3.057 2.066L17.65 15.35q-1.125-1.125-2.587-1.737T12 13q-1.6 0-3.062.613T6.35 15.35M2.1 11.1l-1.023-1.023Q3.26 7.883 6.087 6.71q2.828-1.173 5.917-1.173q3.088 0 5.912 1.173q2.824 1.172 5.007 3.366L21.9 11.1q-1.975-1.975-4.537-3.037T12 7Q9.2 7 6.637 8.063T2.1 11.1M12 21l-2.448-2.454q.486-.486 1.119-.747q.633-.26 1.327-.26t1.329.26q.634.26 1.121.747z" />
+</svg>`,
+        },
+
+    ]
 </script>
-<div>
-    Hotels For Booking 
+<div class="flex items-center justify-center flex-col p-5 bg-indigo-100/50">
+    <h1 class="text-2xl md:text-3xl lg:text-5xl font-extrabold p-5 md:p-10 text-center w-[90%] md:w-[80%] rounded bg-blue-200 ">
+        Choose Hotels <br> According Need
+    </h1>
+    <div class="flex flex-col gap-4 items-center w-[90%] md:w-[80%] my-5 ">
+
+        {#each hotels as {name, url,imgUrl}}
+             <div class="w-full lg:w-[80%] flex flex-col md:flex-row shadow-lg rounded-lg overflow-hidden bg-white ">
+               <div class="w-full lg:w-1/3 flex items-center justify-center">
+                <img class=" w-full" src={imgUrl} alt={name} >
+               </div>
+               <div class="w-2/3 p-2 flex flex-col md:flex-row ">
+                    <div class="w-full lg:w-2/3">
+                        <h3 class="font-bold text-lg lg:text-xl">MIRACLE TRANSIT HOTEL</h3>
+                        <p class="text-xs lg:text-sm text-black/70 font-light w-[90%] flex ">
+                            <span class="mt-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+                                    <path fill="currentColor" d="M12 11.5A2.5 2.5 0 0 1 9.5 9A2.5 2.5 0 0 1 12 6.5A2.5 2.5 0 0 1 14.5 9a2.5 2.5 0 0 1-2.5 2.5M12 2a7 7 0 0 0-7 7c0 5.25 7 13 7 13s7-7.75 7-13a7 7 0 0 0-7-7" />
+                                </svg>
+                            </span>
+                             3rd Floor, International Departure Hall, Concourse A, Suvarnabhumi Airport. Bangkok
+                        </p>
+                        <div class="flex  flex-wrap py-2 w-[90%] gap-1">
+                            {#each facilities as {name,icon}}
+                                 <p class="text-xs flex  items-center gap-1 capitalize font-light px-2 py-1 border border-cyan-400 rounded">
+                                    <span>{@html icon }</span>
+                                    <span>{ name }</span>
+                                 </p>
+                            {/each}
+                        </div>
+                        <p class="text-black/80 font-light">The Miracle Transit Hotel offers you the possibility to book a room from 4 to 12 hours.</p>
+                    </div>
+                    <div class="w-full lg:w-1/3 flex flex-col  md:items-end p-3">
+                        <p>Hotel Reviews </p>
+                        <p class="flex">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64" {...$$props}>
+                                <path fill="#ffce31" d="M62 25.2H39.1L32 3l-7.1 22.2H2l18.5 13.7l-7 22.1L32 47.3L50.5 61l-7.1-22.2z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64" {...$$props}>
+                                <path fill="#ffce31" d="M62 25.2H39.1L32 3l-7.1 22.2H2l18.5 13.7l-7 22.1L32 47.3L50.5 61l-7.1-22.2z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64" {...$$props}>
+                                <path fill="#ffce31" d="M62 25.2H39.1L32 3l-7.1 22.2H2l18.5 13.7l-7 22.1L32 47.3L50.5 61l-7.1-22.2z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64" {...$$props}>
+                                <path fill="#ffce31" d="M62 25.2H39.1L32 3l-7.1 22.2H2l18.5 13.7l-7 22.1L32 47.3L50.5 61l-7.1-22.2z" />
+                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64" {...$$props}>
+                                <path fill="#ffce31" d="M62 25.2H39.1L32 3l-7.1 22.2H2l18.5 13.7l-7 22.1L32 47.3L50.5 61l-7.1-22.2z" />
+                            </svg>
+                            <span>
+                                (132)
+                            </span>
+                        </p>
+
+                        <div class=" text-center">
+                            <p class="font-bold">Price Starts At</p>
+                            <p class="text-red-500 text-lg font-semibold">BDT 12,345 </p>
+                            <p class="  line-through">BDT 12,345 </p>
+                        </div> 
+                        <div class="flex lg:flex-col gap-2 text-center ">
+
+                            <a href="/" class="border shadow bg-green-500 px-3 py-2   rounded-lg hover:bg-green-600">Call Now</a> 
+                            <a href="/" class="border shadow bg-blue-500 px-3 py-2   rounded-lg hover:bg-sky-600">View Details</a> 
+                        </div>
+                    </div>
+               </div>
+             </div>
+        {/each}
+    </div>
 </div>
