7c5198d5aa2524666422e7b4a068cb07f8ad8a3c
kaiesmahmud <kaiesmahmud@gmail.com>
2024-03-21 01:58:42 +0600

    data added

---
+++ src/lib/constants/touringPlaces.js
@@ -0,0 +1,27 @@
+export let touringPlaces = [
+    {
+        name:"Aquaholic Tourist Caravan", 
+        url:"/",
+        imgUrl: "https://gozayaan.sgp1.digitaloceanspaces.com/media/tour_management/pictures/upload/9cbefe74-7cf9-44ab-ba3c-4f7c598991ad.jpg",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://gozayaan.sgp1.digitaloceanspaces.com/media/tour_management/pictures/upload/9cbefe74-7cf9-44ab-ba3c-4f7c598991ad.jpg",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://gozayaan.sgp1.digitaloceanspaces.com/media/tour_management/pictures/upload/9cbefe74-7cf9-44ab-ba3c-4f7c598991ad.jpg",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://gozayaan.sgp1.digitaloceanspaces.com/media/tour_management/pictures/upload/9cbefe74-7cf9-44ab-ba3c-4f7c598991ad.jpg",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://gozayaan.sgp1.digitaloceanspaces.com/media/tour_management/pictures/upload/9cbefe74-7cf9-44ab-ba3c-4f7c598991ad.jpg",
+    },
+]
---
+++ src/lib/constants/travelTickets.js
@@ -0,0 +1,27 @@
+export let travelTickets =  [
+    {
+        name:"Aquaholic Tourist Caravan", 
+        url:"/",
+        imgUrl: "https://plus.unsplash.com/premium_photo-1661713771405-09e2b484fdc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG91ciUyMGJ1c3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://plus.unsplash.com/premium_photo-1661713771405-09e2b484fdc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG91ciUyMGJ1c3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://plus.unsplash.com/premium_photo-1661713771405-09e2b484fdc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG91ciUyMGJ1c3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://plus.unsplash.com/premium_photo-1661713771405-09e2b484fdc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG91ciUyMGJ1c3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+        name:"Aquaholic Tourist Caravan",
+        url:"/",
+        imgUrl: "https://plus.unsplash.com/premium_photo-1661713771405-09e2b484fdc8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dG91ciUyMGJ1c3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+]
